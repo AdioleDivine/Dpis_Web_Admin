5657276dfce99d1ce54f552db8d675c47d2b6ace
AdioleDivine <adioledivine27@Gmail.com>
2020-11-10 00:29:34 +0100

    Responsive

--- lib/Home.dart
+++ lib/Home.dart
@@ -21,16 +21,48 @@ class HomePage extends StatelessWidget {
         ),
         duration: Duration(milliseconds: 30),
       );
+
+  adminView(context) => AnimatedContainer(
+        color: Colors.white,
+        height: ResponsiveWidget.isSmallScreen(context)
+            ? MediaQuery.of(context).size.height * 0.60
+            : MediaQuery.of(context).size.width * 0.30,
+        width: ResponsiveWidget.isSmallScreen(context)
+            ? MediaQuery.of(context).size.height * 0.60
+            : MediaQuery.of(context).size.width * 0.50,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("ADMIN LOGIN"),
+            ),
+          ],
+        ),
+        duration: Duration(milliseconds: 30),
+      );
   const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[900],
-      body: Column(
-        children: [
-          Align(alignment: Alignment.topCenter, child: profileImage(context))
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.topCenter,
+                child: Column(children: [
+                  profileImage(context),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height / 10,
+                  ),
+                  adminView(context),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height / 10,
+                  ),
+                ]))
+          ],
+        ),
       ),
     );
   }
